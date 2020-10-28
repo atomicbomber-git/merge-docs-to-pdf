c9de22093ce98177e0695e24cd31e5623b37c6d9
Antonius Yonathan <atomicbomber.email@gmail.com>
2020-10-28 16:20:35 +0700

    Updates.

--- main.jl
+++ main.jl
@@ -1,12 +1,126 @@
+import JSON
+
 if (length(ARGS) != 1) || (!isdir(ARGS[1]))
     println(stderr::IO, "This program requires at least 1 argument, that is the root directory of the place where the documents are stored")
     exit(1)
 end
 
-filenames = readdir(ARGS[1])
+function clean_filename(filename)
+    cleaned = lowercase(filename)
+    cleaned = replace(cleaned, " " => "_")
+end
+
+root_path = ARGS[1]
+filenames = readdir(root_path)
+
+mahasiswa_names = Array{String,1}()
+contains_bab = Dict{String,Array{String,1}}()
+contains_skripsi = Dict{String,Array{String,1}}()
+contains_remainder = Dict{String,Array{String,1}}()
 
 for filename in filenames
-    lowercased = lowercase(filename)
-    cleaned = replace(lowercased, " " => "_")
-    println(cleaned)
+
+    subdir_path = joinpath(root_path, filename)
+    subfilenames = readdir(subdir_path)
+
+    for subfilename in subfilenames
+        full_subfilename = joinpath(subdir_path, subfilename)
+        
+        if (!isdir(full_subfilename))
+            continue
+        end
+
+        append!(mahasiswa_names, [subfilename])
+
+        # Check for files that contain BAB
+        for (root, dir, files) in walkdir(full_subfilename)
+            for file in files
+
+                joined_filename = joinpath(root, file)
+
+                if (findfirst("bab", lowercase(file)) !== nothing)
+                    if (!haskey(contains_bab, subfilename))
+                        contains_bab[subfilename] = Array{String,1}()
+                    end
+
+                    append!(contains_bab[subfilename], [joined_filename])
+                
+                elseif (
+                    ( findfirst("skripsi", lowercase(file)) !== nothing ) ||
+                    ( findfirst("tugas akhir", lowercase(file)) !== nothing )
+                )
+                    if (!haskey(contains_skripsi, subfilename))
+                        contains_skripsi[subfilename] = Array{String,1}()
+                    end
+
+                    append!(contains_skripsi[subfilename], [joined_filename])
+                else
+                    if (!haskey(contains_remainder, subfilename))
+                        contains_remainder[subfilename] = Array{String,1}()
+                    end
+                    
+                    append!(contains_remainder[subfilename], [joined_filename])
+                end
+            end
+        end
+    end
+end
+
+
+final_catalogue = Dict{String, Array{String, 1}}()
+
+prioritized_extensions = [".pdf", ".docx", ".doc"]
+
+for mahasiswa_name in mahasiswa_names
+    final_catalogue[mahasiswa_name] = Array{String,1}()
+
+    local filenames = get(contains_skripsi, mahasiswa_name, nothing)
+    if (filenames !== nothing)
+        for extension in prioritized_extensions
+            for filename in filenames
+                if (splitext(filename)[2] === extension)
+                    
+                    append!(
+                        final_catalogue[mahasiswa_name],
+                        [filename]
+                    )
+                    break
+                end
+            end
+        end 
+    end
+    
+    if (length(final_catalogue[mahasiswa_name]) !== 0)
+        continue
+    end
+
+    filenames = get(contains_bab, mahasiswa_name, nothing)
+    if (filenames !== nothing)
+        for extension in prioritized_extensions
+            temp = Array{String,1}()
+            for filename in filenames
+                if (splitext(filename)[2] === extension)
+                    append!(
+                        temp,
+                        [filename]
+                    )
+                end
+            end
+
+            if (length(temp) !== 0)
+                append!(final_catalogue[mahasiswa_name], temp)
+                break
+            end
+        end
+    end
+
+    if (length(final_catalogue[mahasiswa_name]) !== 0)
+        continue
+    end
+end
+
+for (key, value) in final_catalogue
+    println(key)
+    println(value)
+    println()
 end
---
+++ saracastic.jl
@@ -0,0 +1,20 @@
+
+
+# awesome = Dict{String, Array{String}}()
+
+# awesome["TEST"] = ["Hello world"]
+
+# # append!(awesome["TEST"], "COCK")
+
+# println(awesome)
+
+testarray = Array{String, 1}()
+
+# println(testarray)
+
+
+append!(testarray, ["TEST"])
+println(testarray)
+
+
+# println(testarray)
